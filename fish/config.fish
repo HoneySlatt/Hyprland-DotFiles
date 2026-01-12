fba4b8cc05e8d29283d17476da4b5de53c5f92e5
Savané Fofana <HoneySNK@proton.me>
2026-01-12 00:20:47 +0000

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+# Fish Configuration
+# by Saifullah Balghari
+# -----------------------------------------------------
+
+# Remove the fish greetings
+set -g fish_greeting
+
+# Start neofetch
+neofetch
